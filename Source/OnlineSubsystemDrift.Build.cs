44191aeb0ef6576bb9813c4c1ca782b9faebe42d
Andreas Axelsson <andreas.axelsson@gmail.com>
2017-07-12 08:22:03 +0800

    Update copyright header in all source files

--- Source/OnlineSubsystemDrift.Build.cs
+++ Source/OnlineSubsystemDrift.Build.cs
@@ -1,4 +1,14 @@
-// Copyright 2015-2017 Directive Games Limited - All Rights Reserved
+/**
+* This file is part of the Drift Unreal Engine Integration.
+*
+* Copyright (C) 2016-2017 Directive Games Limited. All Rights Reserved.
+*
+* Licensed under the MIT License (the "License");
+*
+* You may not use this file except in compliance with the License.
+* You may obtain a copy of the license in the LICENSE file found at the top
+* level directory of this module, and at https://mit-license.org/
+*/
 
 using UnrealBuildTool;
 
