d660002f4dadefbbc3057107b6f08ec2ed709a89
Andreas Axelsson <axl@directivegames.com>
2020-10-16 16:09:32 +0800

    UpdateSession now updates the Drift Match as well
    - game mode, map name, and max players

--- Source/Private/OnlineSessionDrift.cpp
+++ Source/Private/OnlineSessionDrift.cpp
@@ -382,17 +382,47 @@ bool FOnlineSessionDrift::StartSession(FName SessionName)
 
 bool FOnlineSessionDrift::UpdateSession(FName SessionName, FOnlineSessionSettings& UpdatedSessionSettings, bool bShouldRefreshOnlineData)
 {
-    bool bWasSuccessful = true;
+    uint32 Result = ONLINE_FAIL;
 
     auto Session = GetNamedSession(SessionName);
     if (Session)
     {
-        // TODO: What should this do?
+    	if (auto Drift = DriftSubsystem->GetDrift())
+    	{
+    		FDriftUpdateMatchProperties Properties{};
+    		FString mapName;
+    		if (Session->SessionSettings.Get(TEXT("map_name"), mapName))
+    		{
+    			Properties.mapName = mapName;
+    		}
+    		FString gameMode;
+    		if (Session->SessionSettings.Get(TEXT("game_mode"), gameMode))
+    		{
+    			Properties.gameMode = gameMode;
+    		}
+    		if (Session->SessionSettings.NumPublicConnections != UpdatedSessionSettings.NumPublicConnections)
+    		{
+    			Properties.maxPlayers = UpdatedSessionSettings.NumPublicConnections;
+    		}
+    		Drift->UpdateMatch(Properties, FDriftMatchStatusUpdatedDelegate::CreateLambda([this, SessionName](bool success)
+    		{
+		        TriggerOnUpdateSessionCompleteDelegates(SessionName, success);
+    		}));
+            Result = ONLINE_IO_PENDING;
+    	}
         Session->SessionSettings = UpdatedSessionSettings;
-        TriggerOnUpdateSessionCompleteDelegates(SessionName, bWasSuccessful);
+    }
+    else
+    {
+        UE_LOG_ONLINE(Warning, TEXT("Can't update an online game for session (%s) that hasn't been created"), *SessionName.ToString());
     }
 
-    return bWasSuccessful;
+    if (Result != ONLINE_IO_PENDING)
+    {
+        TriggerOnUpdateSessionCompleteDelegates(SessionName, (Result == ONLINE_SUCCESS) ? true : false);
+    }
+
+	return Result == ONLINE_SUCCESS || Result == ONLINE_IO_PENDING;
 }
 
 bool FOnlineSessionDrift::EndSession(FName SessionName)
