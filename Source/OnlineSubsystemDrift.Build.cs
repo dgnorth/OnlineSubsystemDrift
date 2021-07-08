bfdfdcefce447247b26a1e231818ea410e4f8f7a
Hilmar Tryggvason <hilmart@gmail.com>
2021-07-08 14:54:55 +0000

    Include JsonArchive in build.cs

--- Source/OnlineSubsystemDrift.Build.cs
+++ Source/OnlineSubsystemDrift.Build.cs
@@ -58,6 +58,7 @@ public class OnlineSubsystemDrift : ModuleRules
                 "Voice",
                 "OnlineSubsystem",
                 "Drift",
+                "JsonArchive",
 			}
 			);
 	}
